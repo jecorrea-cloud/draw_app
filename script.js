f2521b945fbb986777a0c22ced3d52dabce6ee70
joaquin <jecorrea@protonmail.com>
2022-01-25 01:46:41 -0500

    basic JS

---
+++ script.js
@@ -0,0 +1,8 @@
+// Bring down all the HTML elements
+
+const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEL = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
