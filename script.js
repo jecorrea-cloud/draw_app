482474712c347107c00e481d4e029ab895556c1b
joaquin <jecorrea@protonmail.com>
2022-01-25 02:04:44 -0500

    drawLine function added

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ const ctx = canvas.getContext("2d");
 
 // Draw a circle
 
-let size = 20;
-let color = black;
+let size = 10;
+colorEl.value = "black";
+let color = colorEl.value;
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
 function drawCircle(x, y) {
@@ -26,4 +27,14 @@ function drawCircle(x, y) {
 
 // Draw a line
 
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
+}
+
 drawCircle(100, 200);
+drawLine(300, 300, 200, 500);
